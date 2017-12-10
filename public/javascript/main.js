361cb019b0809c11dd093c9bd7bbe34fd8e28d8c
tahbristol <tahbristol@gmail.com>
2017-12-10 01:04:03 -0500

    api fetch working

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,6 @@
 'use-strict'
-
-let url = `https://www.googleapis.com/civicinfo/v2/representatives?address=4027 vistaview street west mifflin pa&key=${ENV['GOOGLE_API_KEY']}`
+const API_KEY = "AIzaSyD5JWZW3JJSHUYyE8wKCLUOnesa5Udd1AI";
+let url = `https://www.googleapis.com/civicinfo/v2/representatives?address=4027 vistaview street west mifflin pa&key=${API_KEY}`
 //  let url =  'https://jsonplaceholder.typicode.com/posts/1';
 var civicData;
 fetch(url)
